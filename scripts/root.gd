92485aa0705001fe788f8996e966233ecb17a3f9
Tom Beckmann <tomjonabc@gmail.com>
2023-05-03 15:41:53 +0200

    Restructure rooms

--- scripts/root.gd
+++ scripts/root.gd
@@ -7,7 +7,17 @@ func _ready():
 	if xr_interface and xr_interface.is_initialized():
 		var vp: Viewport = get_viewport()
 		vp.use_xr = true
-
+	right_hand.button_pressed.connect(open_room)
+		
+	
+		
+@onready var right_hand : XRController3D = $"XROrigin3D/Right Hand"
+		
+func open_room(name):
+	if name == "ax_button":
+		var woods_room_resource: PackedScene = preload("res://scenes/rooms/woods_room.tscn")
+		var new_room = woods_room_resource.instantiate()
+		self.add_child(new_room)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
