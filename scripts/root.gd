68009e3995445de8aad293466505d72d8643a301
frcroth <frcroth@gmail.com>
2023-05-18 11:46:37 +0200

    Add visual room switcher menu

--- scripts/root.gd
+++ scripts/root.gd
@@ -7,17 +7,18 @@ func _ready():
 	if xr_interface and xr_interface.is_initialized():
 		var vp: Viewport = get_viewport()
 		vp.use_xr = true
-	right_hand.button_pressed.connect(open_room)
+	right_hand.button_pressed.connect(on_right_hand_button_pressed)
 		
 	
 		
 @onready var right_hand : XRController3D = $"XROrigin3D/Right Hand"
 		
-func open_room(name):
-	if name == "ax_button":
-		var woods_room_resource: PackedScene = preload("res://rooms/Adventure/woods_room.tscn")
-		var new_room = woods_room_resource.instantiate()
-		self.add_child(new_room)
+func on_right_hand_button_pressed(button_name):
+	if button_name == "ax_button":
+		#var woods_room_resource: PackedScene = preload("res://rooms/Adventure/woods_room.tscn")
+		#var new_room = woods_room_resource.instantiate()
+		#self.add_child(new_room)
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
