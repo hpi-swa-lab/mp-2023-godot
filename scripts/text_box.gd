bf45c64221fae17f647643de8f5ccbc91b0f325b
frcroth <frcroth@gmail.com>
2023-06-29 11:52:12 +0200

    Fix startup issues

--- scripts/text_box.gd
+++ scripts/text_box.gd
@@ -7,54 +7,63 @@ enum TextAlignment {
 	RIGHT
 }
 
-@export_multiline var text: String : set = set_text
-@export var resize_box: bool = true
-@export var depth: float = 0.1 : set = set_depth
+@export_multiline var text: String: 
+	set(s):
+		text = s
+		apply_properties()
+
+@export var resize_box: bool:
+	set(r):
+		resize_box = r
+		apply_properties()
+
+@export var depth: float = 0.1 :
+	set(d):
+		depth = d
+		apply_properties()
+
 @export var height: float:
 	set(h):
 		height = h
-		if not is_inside_tree(): await ready
-		box_mesh.size.y = h
-		collision_box.size = box_mesh.size
-	get:
-		if not is_inside_tree(): await ready
-		return box_mesh.size.y
+		apply_properties()
+
 @export var width: float:
 	set(w):
 		width = w
-		if not is_inside_tree(): await ready
-		box_mesh.size.x = w
-		collision_box.size = box_mesh.size
-	get:
-		if not is_inside_tree(): await ready
-		return box_mesh.size.x
-@export var material: Material : set = set_material
-@export var text_font_size : float : set = set_text_font_size
+		apply_properties()
+
+@export var material: Material :
+	set(m):
+		material = m
+		apply_properties()
+
+@export var text_font_size : float :
+	set(t):
+		text_font_size = t
+		apply_properties()
+
 @export var alignment: TextAlignment = TextAlignment.CENTER :
 	set(new_alignment):
-		if !Engine.is_editor_hint():
-			return
 		alignment = new_alignment
-		if not is_inside_tree(): await ready
-		label.horizontal_alignment = int(alignment)
-		if resize_box:
-			rearrange()
+		apply_properties()
+		
 @export var enable_handle_bar: bool = true:
-	set(enabled):
-		if !Engine.is_editor_hint():
-			return
-		if not is_inside_tree(): await ready
-		handle.visible = enabled
-		handle.enabled = enabled
-		handle.collision_layer = 0b00000000_00010000_00000000_00000000 if enabled else 0
-		enable_handle_bar = enabled
+	set(e):	
+		enable_handle_bar = e
+		apply_properties()
 
 @onready var label: Label3D = $Label
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
 
-@export var margin_x = 0.02
-@export var margin_y = 0.02
+@export var margin_x = 0.02:
+	set(m):
+		margin_x = m
+		apply_properties()
+@export var margin_y = 0.02:
+	set(m):
+		margin_y = m
+		apply_properties()
 
 var z_dist = 0.0001
 
@@ -65,29 +74,6 @@ var z_dist = 0.0001
 @onready var handle_mesh: MeshInstance3D = $"InteractableHandle/MeshInstance3D"
 @onready var handle_collision_shape: CollisionShape3D = $"InteractableHandle/CollisionShape3D"
 
-#@export var debug_redraw = false:
-#	set(do_redraw):
-#		if not is_inside_tree(): await ready
-#		debug_redraw = do_redraw
-#		var h = height
-#		var w = width
-#		var d = depth
-#		var r = resize_box
-#		var m = material
-#		var t = text_font_size
-#		var a = alignment
-#		resize_box = true
-#		text = text
-#		material = m
-#		text_font_size = t
-#		alignment = a
-#		rearrange()
-#		depth = d
-#		width = w
-#		height = h
-#		resize_box = r
-		
-
 signal pointer_entered
 signal pointer_exited
 
@@ -95,6 +81,7 @@ signal pointer_exited
 func _ready():
 	pointer_entered.connect(on_pointer_entered)
 	pointer_exited.connect(on_pointer_exited)
+	apply_properties()
 
 func on_pointer_entered():
 	G.add_outline(mesh)
@@ -102,8 +89,38 @@ func on_pointer_entered():
 func on_pointer_exited():
 	G.remove_outline(mesh)
 
+var in_update = false
+
+func apply_properties():
+	if not is_inside_tree():
+		if !Engine.is_editor_hint():
+			return
+		else:
+			await ready
+	if in_update: return
+	in_update = true
+
+	box_mesh.size.y = height
+	box_mesh.size.x = width
+	collision_box.size = box_mesh.size
+
+	label.horizontal_alignment = int(alignment)
+
+	handle.visible = enable_handle_bar
+	handle.enabled = enable_handle_bar
+	handle.collision_layer = 0b00000000_00010000_00000000_00000000 if enable_handle_bar else 0
+
+	label.text = text
+	label.font_size = text_font_size
+
+	box_mesh.material = material
+
+	if resize_box:
+		rearrange()
+	in_update = false
+
 func rearrange():
-	if not is_inside_tree(): await ready
+	if not is_inside_tree(): return
 	var bb = label.get_aabb().size
 	box_mesh.size = Vector3(bb.x + margin_x, bb.y + margin_y, depth)
 	collision_box.size = box_mesh.size
@@ -113,27 +130,12 @@ func rearrange():
 		label.position.x = bb.x / 2.0
 	else:
 		label.position.x = 0
+	height = box_mesh.size.y
+	width = box_mesh.size.x
 	
 	reposition_label()
 	reposition_handle(bb)
 
-func set_depth(d):
-	if !Engine.is_editor_hint():
-		return
-	
-	depth = d
-	rearrange()
-
-func set_text(s):
-	if !Engine.is_editor_hint():
-		return
-	
-	text = s
-	if not is_inside_tree(): await ready
-	label.text = s
-	if resize_box:
-		rearrange()
-		
 func reposition_label():
 	label.position.z = box_mesh.size.z / 2 + z_dist
 
@@ -148,15 +150,6 @@ func set_material(m):
 	if !Engine.is_editor_hint():
 		return
 	material = m
-	if not is_inside_tree(): await ready
+	if not is_inside_tree(): return
 	box_mesh.material = m
 	
-func set_text_font_size(s):
-	if !Engine.is_editor_hint():
-		return
-	
-	text_font_size = s
-	if not is_inside_tree(): await ready
-	label.font_size = s
-	if resize_box:
-		rearrange()
