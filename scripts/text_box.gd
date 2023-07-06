bda4aec0a7a602a5807d36890a68be5460cc78cb
frcroth <frcroth@gmail.com>
2023-07-06 14:06:09 +0200

    Rewort text-box, make non pickable add 3DDMM label

--- scripts/text_box.gd
+++ scripts/text_box.gd
@@ -1,10 +1,10 @@
 @tool
-extends XRToolsPickable
+extends StaticBody3D
 
 enum TextAlignment {
 	LEFT,
 	CENTER,
-	RIGHT
+	RIGHT	
 }
 
 @export_multiline var text: String: 
@@ -37,9 +37,9 @@ enum TextAlignment {
 		material = m
 		apply_properties()
 
-@export var text_font_size : float :
-	set(t):
-		text_font_size = t
+@export var intended_viewing_distance : float :
+	set(i):
+		intended_viewing_distance = i
 		apply_properties()
 
 @export var alignment: TextAlignment = TextAlignment.CENTER :
@@ -57,7 +57,7 @@ enum TextAlignment {
 		hide_box = h
 		apply_properties()
 
-@onready var label: Label3D = $Label
+@onready var label: Label3DDMM = $Label3DDMM
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 @onready var collision_shape: CollisionShape3D = $CollisionShape3D
 
@@ -115,6 +115,7 @@ func apply_properties():
 
 	box_mesh.size.y = height
 	box_mesh.size.x = width
+	box_mesh.size.z = depth
 	collision_box.size = box_mesh.size
 
 	label.horizontal_alignment = int(alignment)
@@ -124,7 +125,7 @@ func apply_properties():
 	handle.collision_layer = 0b00000000_00010000_00000000_00000000 if enable_handle_bar else 0
 
 	label.text = text
-	label.font_size = text_font_size
+	label.intended_viewing_distance = intended_viewing_distance
 
 	box_mesh.material = material
 	mesh.visible = !hide_box
